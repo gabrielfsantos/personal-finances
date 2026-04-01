98f55faf610646803a7fceff5f9f1af1729f540a
Gabriel Santos <gabrielclimber@gmail.com>
2026-04-01 16:32:12 -0300

    add gtest to the project

--- core/src/tests/transaction_tests.cpp
+++ core/src/tests/transaction_tests.cpp
@@ -1,12 +1,18 @@
-#include <cassert>
+#include <gtest/gtest.h>
 
 #include "core/src/transaction.h"
 
-int main() {
+TEST(TransactionTest, CreatesValidTransaction) {
     Transaction t(TransactionType::Income, Money(100, Currency::EUR), "food", 12345, "Lunch");
 
-    assert(t.id.size() == 32);
-    assert(t.amount.amount == 100);
+    EXPECT_EQ(t.amount.amount, 100);
+    EXPECT_EQ(t.category_id, "food");
+    EXPECT_EQ(t.description.value(), "Lunch");
+    EXPECT_EQ(t.type, TransactionType::Income);
+}
 
-    return 0;
+TEST(TransactionTest, Generates32CharId) {
+    Transaction t(TransactionType::Expense, Money(50, Currency::EUR), "transport", 99999, "Bus");
+
+    EXPECT_EQ(t.id.size(), 32);
 }
